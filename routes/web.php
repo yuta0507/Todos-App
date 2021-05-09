70d9eccef675c33c3f17d6a1d92873225c8079dd
吉川祐大 <yuta@yoshikawayuudainoMacBook-Pro.local>
2021-05-09 21:53:40 +0900

    Implementation of flash messaging and completing todos functions +α

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,10 @@ Route::get('todos', [Controllers\TodosController::class, 'index']);
 Route::get('todos/{todo}', [Controllers\TodosController::class, 'show']);
 Route::get('new-todos', [Controllers\TodosController::class, 'create']);
 Route::post('store-todos', [Controllers\TodosController::class, 'store']);
+
 Route::get('todos/{todo}/edit', [Controllers\TodosController::class, 'edit']);
 Route::post('todos/{todo}/update-todos', [Controllers\TodosController::class, 'update']);
+
+Route::get('todos/{todo}/delete', [Controllers\TodosController::class, 'destroy']);
+
+Route::get('todos/{todo}/complete', [Controllers\TodosController::class, 'complete']);
